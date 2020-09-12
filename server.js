c5180c2a64d24675d48d9eb98ad37de4b03554b9
Robotic7o7 <scientificrohan@gmail.com>
2020-09-12 16:10:47 +0530

    initial commit

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const exphbs=require ('express-handlebars');
 const bodyparser = require('body-parser');
 const cors = require ('cors');
 
+
 const employeeController = require('./controllers/employeeController');
 var app = express();
 //use function for middleware
@@ -22,10 +23,6 @@ app.use(cors())
 app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/pages/home.html")
-});
 
 app.listen(3000, () => {
     console.log('Express server started at port : 3000');
